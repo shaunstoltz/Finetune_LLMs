1fd9805938f32a41f7281f9955e505d8ef9e3da4
Shaun Stoltz <shaunstoltz@hotmail.com>
2023-11-19 10:05:53 -0500

    mii eval

--- eval/eval_mii.py
+++ eval/eval_mii.py
@@ -2,10 +2,10 @@ import os
 import json
 
 import argparse
+
 import datetime
 
 import time
-
 
 results = {
     "start": 0,
@@ -26,125 +26,28 @@ results = {
     }]
 }
 
-#transformers inference ########################################################################################################################
+# vLLM inference #####################################################
 
-import argparse
-from datasets import load_dataset
-import os
-from peft import LoraConfig
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed, BitsAndBytesConfig
+from mii import pipeline
 
+# prompts = [
+#     "Hello, my name is",
+#     "The president of the United States is",
+#     "The capital of France is",
+#     "The future of AI is",
+# ]
+# sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
+# llm = LLM(model="/mnt/md1/shaun/repos/ai/models/yi6bfulldatasetfinalcp/", trust_remote_code=True)
 
+# outputs = llm.generate(prompts, sampling_params)
 
+# Print the outputs.
+# for output in outputs:
+#     prompt = output.prompt
+#     generated_text = output.outputs[0].text
+#     print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
 
-def load_model(model_name, bnb_config):
-    n_gpus = torch.cuda.device_count()
-    max_memory = f'{10000}MB'
-
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        trust_remote_code=True,
-        torch_dtype=torch.bfloat16,
-        device_map = 0,
-        #quantization_config=bnb_config,
-        device_map="auto", # dispatch efficiently the model on the available ressources
-        max_memory = {i: max_memory for i in range(n_gpus)},
-    )
-    tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=True, trust_remote_code=True,)
-
-    # Needed for LLaMA tokenizer
-    tokenizer.pad_token = tokenizer.eos_token
-
-    return model, tokenizer
-
-
-def get_max_length(model):
-    conf = model.config
-    max_length = None
-    for length_setting in ["n_positions", "max_position_embeddings", "seq_length"]:
-        max_length = getattr(model.config, length_setting, None)
-        if max_length:
-            print(f"Found max lenth: {max_length}")
-            break
-    if not max_length:
-        max_length = 1024
-        print(f"Using default max length: {max_length}")
-    return max_length
-
-
-def create_bnb_config():
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16,
-    )
-
-    return bnb_config
-
-def create_peft_config(modules):
-    """
-    Create Parameter-Efficient Fine-Tuning config for your model
-    :param modules: Names of the modules to apply Lora to
-    """
-    config = LoraConfig(
-        r=16,  # dimension of the updated matrices
-        lora_alpha=64,  # parameter for scaling
-        target_modules=modules,
-        lora_dropout=0.1,  # dropout probability for layers
-        bias="none",
-        task_type="CAUSAL_LM",
-    )
-
-    return config
-
-
-
-
-
-
-
-#end transformer interence ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-def query(messages: "list[dict]", max_tokens: int, api_key: str, temperature: float, top_p: float, sleep_time: float, model_name: str, ground_truth: str, full_dataset_item: dict) -> str:
-
-    time.sleep(sleep_time)
-    try:
-        if model_name in support_models:
-            response = openai.ChatCompletion.create(
-                model=model_name,
-                messages=messages,
-                temperature=temperature,
-                top_p=top_p,
-                max_tokens=max_tokens,
-                api_key=api_key,
-            )
-            
-            result = {
-                    "ground_truth": ground_truth,
-                    "full_dataset_item": full_dataset_item,
-                    "gpt_interaction": {
-                        "api_call": {
-                            "prompt": message,
-                            "response": response
-                        }
-                    }
-            }
-            
-
-            results['outcomes'].append(result)
-
-            gen = response['choices'][0]['message']['content']
-            #print(gen, response)
-        return gen
-    except Exception as e:
-        print(e)
 
 
 def parse_args():
@@ -198,8 +101,8 @@ def get_MATH_data_set_array(directory):
     json_files = [pos_json for pos_json in os.listdir(directory) if pos_json.endswith('.json')]
     return json_files
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     current_script_path = os.path.abspath(__file__)
     MAD_path = current_script_path.rsplit("/", 1)[0]
 
@@ -224,8 +127,6 @@ if __name__ == "__main__":
     post = args.post
     post_prompt = ""
     prompt = ""
-
-
 
     if input_dataset == "MATH":
         files = get_MATH_data_set_array(input_file)
@@ -273,30 +174,10 @@ if __name__ == "__main__":
     if input_dataset == "gsm8k":
         if input_file is not None:
             with open(input_file) as f:
-                data = [json.loads(line) for line in f] 
+                data = [json.loads(line) for line in f]    
 
-    # Reproducibility
-    seed = 42
-    set_seed(seed)
-
-    output_dir = "results/7b/final_checkpoint"
-    os.makedirs(output_dir, exist_ok=True)
-    output_merged_dir = "results/7b/final_merged_checkpoint"
-    os.makedirs(output_merged_dir, exist_ok=True)
-
-    model_name = "EleutherAI/llemma_7b" 
-    model_name = "HuggingFaceH4/zephyr-7b-beta"
-    model_name="/mnt/md1/shaun/repos/ai/models/yi6bfulldatasetfinalcp/"
-    
-    data = data[int(start):int(start + number)]
-    bnb_config = create_bnb_config()
-    model, tokenizer = load_model(model_name, bnb_config)
-
-    # Specify device
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    print(len(data))
-
+    pipe = pipeline(model_name)
+    #llm = LLM(model=model_name, trust_remote_code=True, dtype="float16")
     j = 0
     for i in data:
         question = i['question']
@@ -311,17 +192,18 @@ if __name__ == "__main__":
         
         prompt = pre_prompt + question + post_prompt
 
-        # Tokenize input text
-        inputs = tokenizer(prompt, return_tensors="pt").to(device)
 
         # Get answer
         # (Adjust max_new_tokens variable as you wish (maximum number of tokens the model can generate to answer the input))
-        outputs = model.generate(input_ids=inputs["input_ids"].to(device), attention_mask=inputs["attention_mask"], max_new_tokens=500, pad_token_id=tokenizer.eos_token_id)
+        #outputs = model.generate(input_ids=inputs["input_ids"].to(device), attention_mask=inputs["attention_mask"], max_new_tokens=500, pad_token_id=tokenizer.eos_token_id)
+        output = pipe(prompt, max_new_tokens=1024)
 
         # Decode output & print it
         #print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 
-        response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        response = output
+
+        print(response)
 
         result = {
             "ground_truth": answer,
@@ -363,4 +245,36 @@ if __name__ == "__main__":
     file.write(json.dumps(results))
     file.close()
     print(results)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
